b7a21880dc3d81146bf026f5b1cb314eb5d2318c
Erik Larson <elarson@oberlin.edu>
2012-11-07 22:12:10 -0500

    Fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoPp::Application.config.secret_token = 'fe3115db238a3db752a9d84db885c5d60e449e0a3e24eb15e25e5c6f0529530bc1de49ada1ad085206d2808f3b4b06b9c3bc49fd860947aeda6e782eca38177a'
